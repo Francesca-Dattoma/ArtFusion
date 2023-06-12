b61480ae971f47c31c36dd1573684011e17a3671
9Manuel6 <tedesdchimanuel96@gmial.com>
2023-06-12 13:37:50 +0200

    modificato il form, e trovare una palette di colori

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -15,7 +15,7 @@
 </head>
 <body>
 
-    <x-navbar />
+
 
     {{-- <x-header /> --}}
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,10 @@
 <x-layout>
+    <div class="min-vh-100">
+        <video class="videoC" autoplay loop  src="/media/video2.mp4"></video>
+        {{-- <iframe width="420" height="315" src="/media/video.mp4?autoplay=1&loop=1" frameborder="0" allowfullscreen></iframe> --}}
 
+    </div>
+    <x-navbar  />
     
     
 </x-layout>
