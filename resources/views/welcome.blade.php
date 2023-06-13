e1f8c42c7982a9f7b00408e361b52b45d6f4c1ad
Valeriaciav <valeria.ciavola@gmail.com>
2023-06-13 11:16:48 +0200

    risolta problematica video home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,23 @@
 <x-layout>
-    <div class="min-vh-100">
-        <video class="videoC" autoplay loop  src="/media/video2.mp4"></video>
+    {{-- <div class="container-fluid " >
+        <video autoplay muted loop id="myVideo" class=" videoC">
+            <source src="/media/video2.mp4" type="video/mp4">
+        </video> --}}
+        {{-- <video class="videoC" autoplay loop ><source src="video2.mp4" type="video/mp4">
+        </video> --}}
         {{-- <iframe width="420" height="315" src="/media/video.mp4?autoplay=1&loop=1" frameborder="0" allowfullscreen></iframe> --}}
 
-    </div>
+    {{-- </div> --}} 
+    {{-- <div class="contenitore">
+        <video muted autoplay loop class="videofullscreen">
+          <source src="/media/video.mp4" type="video/mp4">
+        </video>
+    </div> --}}
+    <video autoplay muted loop id="myVideo">
+        <source src="/media/video2.mp4" type="video/mp4">
+      </video>
+
+
     <x-navbar  />
     
     
