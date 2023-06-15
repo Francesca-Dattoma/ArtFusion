3761d2f26abbc9364111f75b3eb74a6e552f8909
Francesca Dattoma <francdatt@gmail.com>
2023-06-15 16:21:06 +0200

    creazione rotta articoli e aggiunta alla navbar

---
+++ resources/views/articles/index.blade.php
@@ -0,0 +1,23 @@
+<x-layout>
+
+    <div class="container vh-min-100">
+        <div class="row justify-content-center">
+
+            
+            <div class="col-12 col-md-8">
+
+                
+
+
+
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+</x-layout>
